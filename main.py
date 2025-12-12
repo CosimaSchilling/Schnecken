1536e31cd5b96354c1ccb09cc493d7b49f12e8e3
Cosima Schilling <cosima.schilling@gmx.de>
2025-12-12 15:55:33 +0100

    ganz viele neue Beispiele

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # als erstes definieren wir eine Variabel
-file_data="helloworld"
+fileData="helloworld"
 
-print(file_data)
+#print(fileData)
 
-#neue Variable definieren
-newvari="Schnekki 1"
+# neue Variable definieren
+newVari="Schnekki 1"
 
-print(file_data "and" newvari)
+#print(fileData + "5" + newVari)
+#print(8+2)
+#print(2**256)
+#type gibt an Art des Worts in Klammern, vor allem für debuggen benutzt, zB float (nicht ganze Zahl), int(eger), str(ing)
+#print(type(8+2)) 
+#print(type(3/2)) #das ist ein float, eine Variable des Typs reelle Zahl
+#print("π")
+
+#neues Projekt um Text auszugeben
+
