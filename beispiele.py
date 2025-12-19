486a0fa51d4868beef9c27cdf7feca382e822b17
CosimaSchilling <dumb.google123@gmail.com>
2025-12-19 16:45:30 +0100

    Leerzeichen für Übersichtlichkeit

--- beispiele.py
+++ beispiele.py
@@ -1,18 +1,18 @@
-# als erstes definieren wir eine Variabel
+#  als erstes definieren wir eine Variabel
 fileData="helloworld"
 
-#print(fileData)
+#  print(fileData)
 
 # neue Variable definieren
 newVari="Schnekki 1"
 
-#print(fileData + "5" + newVari)
-#print(8+2)
-#print(2**256)
-#type gibt an Art des Worts in Klammern, vor allem für debuggen benutzt, zB float (nicht ganze Zahl), int(eger), str(ing)
-#print(type(8+2)) 
-#print(type(3/2)) #das ist ein float, eine Variable des Typs reelle Zahl
-#print("π")
+#  print(fileData + "5" + newVari)
+#  print(8+2)
+#  print(2**256)
+#  type gibt an Art des Worts in Klammern, vor allem für debuggen benutzt, zB float (nicht ganze Zahl), int(eger), str(ing)
+#  print(type(8+2)) 
+#  print(type(3/2)) #das ist ein float, eine Variable des Typs reelle Zahl
+#  print("π")
 
-#neues Projekt um Text auszugeben
+#  neues Projekt um Text auszugeben
 
