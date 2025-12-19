76f05c40189896ba6fdcb17698b9c783f75737ee
CosimaSchilling <dumb.google123@gmail.com>
2025-12-19 14:57:31 +0100

    website Datei dazu und weiter am Python Skript

--- main.py
+++ main.py
@@ -4,17 +4,20 @@
 # ein array ist eine unsortierte Liste oder Reihe
 # sortet list ist ein dict
 
-fileSnales=open("schnecken_florenz") #öffnet die Datei
-fileSnalesLines=fileSnales.readlines() #ließt einzelne Zeilen und packt sie in einen array
-print(type(fileSnalesLines)) #spuckt Zeilen aus (ohne type)
-
+fileSnalesFlorenz=open("schnecken_florenz") #öffnet die Datei
+fileSnalesHeidelberg=open("schnecken_heidelberg") 
+fileSnalesFlorenzLines=fileSnalesFlorenz.readlines() #ließt einzelne Zeilen und packt sie in einen array
+#  print(type(fileSnalesLines)) #spuckt Zeilen aus (ohne type)
+lineNumbers=True 
+print(type(lineNumbers))
 
 #für jedes Element in diesem Array mache das folgende:
 #print dieses Element
 #und als nächstes nummerieren wir anhand der Variable i die einzelnen Elemente
 
 i=1    #muss davor sein sonst erst ab danach gültig
-for schnecke in fileSnalesLines:
+for schnecke in fileSnalesFlorenzLines:
     schnecke=schnecke.strip()  #das hat den Umbruch nach jedem Element weggestript
     print(i, schnecke)
     i+=1
+    
